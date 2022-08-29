baebdd018beea4f29cf92e5104f38b3dc1707164
Erik Autenrieth <e-aut@web.de>
2022-08-29 17:28:55 +0200

    add study files

---
+++ Test/methods.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+def gauss_elimination():
+    return None
+
+def jacobi():
+    return None
+
+def gauss_seidel():
+    return None
+
+def sor():
+    return None
+
+
+def absolut_error(x_true, x):
+    return np.abs(x_true - x)
+
+def relativ_error(x_true, x):
+    return np.abs(x_true - x) / np.abs(x)
